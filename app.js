2933ce0a085620ccfb8a8e1bf5c93801d6d103cd
Aakash <9267aakashsharma@gmail.com>
2020-07-12 21:32:11 +0530

    GET input values

--- app.js
+++ app.js
@@ -5,20 +5,41 @@ var budgetController = (function () {
 
 //UI CONTROLLER
 var uiController = (function () {
-  //Some code
+  var domStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(domStrings.inputType).value, //inc or exp
+        description: document.querySelector(domStrings.inputDescription).value, //description text
+        value: document.querySelector(domStrings.inputValue).value,
+      };
+    },
+    getDOMStrings: function () {
+      return domStrings;
+    },
+  };
 })();
 
 //GLOBAL APP CONTROLLER
 var appController = (function (budgetCtrl, uiCtrl) {
+  var DOM = uiCtrl.getDOMStrings();
   var ctrlAddItem = function () {
     // 1. get input data
+    var input = uiCtrl.getInput();
+    console.log(input);
     // 2. Add item to budget controller
     // 3. Add new item to user interface
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-    console.log("it works");
   };
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", ctrlAddItem);
   document.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
       ctrlAddItem();
